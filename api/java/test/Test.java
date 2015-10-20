89b66d5a56c6d0b45f3baac869b3a8ba3a642df6
shockay <brdong2010@hotmail.com>
2015-10-20 18:23:38 +0800

    test1

---
+++ api/java/test/Test.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import util.DateUtil;
+
+public class Test {
+
+	public static final int SECONDS_PER_YEAR = 60 * 60 * 24 * 365;
+	public static final int SECONDS_PER_MONTH = 60 * 60 * 24 * 30;
+	
+	public static void main(String[] args) {
+		
+		 Calendar c = Calendar.getInstance();
+		 c.setTime(new Date(System.currentTimeMillis()));
+		 c.add(Calendar.SECOND, 3*SECONDS_PER_MONTH);
+		 String overTime = DateUtil.date2String(c.getTime(), "yyyy-MM-dd HH:mm:ss");
+		
+		System.out.println(overTime);
+		
+		
+		
+	}
+	
+}
