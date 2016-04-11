6677ca2aee295592430d0f4b3828f5ed54c30fe8
jumbo <jumbo@10.8.13.162>
2016-04-11 13:12:38 +0800

    log

--- mygit/br2zz/api/java/util/StringUtil.java
+++ mygit/br2zz/api/java/util/StringUtil.java
@@ -1,10 +1,14 @@
 package util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StringUtil {
 
+	private final static Logger log = LoggerFactory.getLogger(StringUtil.class);
+	
 	public static void main(String[] args) {
-		
-
+		log.debug("123");
 	}
 
 }
