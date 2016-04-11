ae4a7cb4e32eda2f90657134445cea7d5daf765b
jumbo <jumbo@10.8.13.162>
2016-04-11 14:12:45 +0800

    log.properties

--- mygit/br2zz/api/java/util/StringUtil.java
+++ mygit/br2zz/api/java/util/StringUtil.java
@@ -8,7 +8,10 @@ public class StringUtil {
 	private final static Logger log = LoggerFactory.getLogger(StringUtil.class);
 	
 	public static void main(String[] args) {
+		log.info("123");
 		log.debug("123");
+		log.warn("123");
+		log.error("123");
 	}
 
 }
