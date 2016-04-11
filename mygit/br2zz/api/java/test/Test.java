6f38716a61a7e370b14d5ed973f750a645cf081b
jumbo <jumbo@10.8.13.162>
2016-04-11 18:24:55 +0800

    compare

--- mygit/br2zz/api/java/test/Test.java
+++ mygit/br2zz/api/java/test/Test.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 import util.DateUtil;
+import util.StringUtil;
 
 public class Test {
 
@@ -20,7 +21,8 @@ public class Test {
 		System.out.println(overTime);
 		
 		//123123
-		
+		int i = StringUtil.CompareString("000123", "001220");
+		System.out.println(i);
 	}
 	
 }
--- mygit/br2zz/api/java/util/StringUtil.java
+++ mygit/br2zz/api/java/util/StringUtil.java
@@ -7,11 +7,24 @@ public class StringUtil {
 
 	private final static Logger log = LoggerFactory.getLogger(StringUtil.class);
 	
-	public static void main(String[] args) {
-		log.info("123");
-		log.debug("123");
-		log.warn("123");
-		log.error("123=============");
-	}
+	private final char value[];
+	
+	public StringUtil() {
+        this.value = new char[0];
+    }
 
+	
+	
+	public static  int CompareString (String string, String anotherString){
+		  return string.compareTo(anotherString);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		log.error("123=============");
+		String str1 = "0012";
+		String str2 = "0013";
+		log.debug(str1.concat(str2));
+	}
 }
